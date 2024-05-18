1909931a20842e30e2a832ae0b0e613b25b67bb7
Erickfer29 <erickdelgado0829@gmail.com>
2024-05-17 23:17:11 -0500

    Config Plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 /* -------------------------- importar dependencias ------------------------- */
 
 import express from 'express'
+import morgan from 'morgan'
+
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import { engine } from 'express-handlebars'
+
 
 /* ----------------------------- Inicializacion ----------------------------- */
 
 const app = express();
+/* evitar colisiones de nombre de variable */
+const __dirname = dirname(fileURLToPath (import.meta.url));
 
 /* --------------------------------- setting -------------------------------- */
 
 app.set('port', process.env.PORT || 3000 )
-
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 /* ------------------------------- Middlewares ------------------------------ */
 
-/* --------------------------------- routes --------------------------------- */
+app.set(morgan('dev'));
+app.set(express.urlencoded({extended: false}));
+app.use(express.json());
 
+/* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res) =>{
+    res.json({"message": "Hola Mundooo"});
+
+});
 /* ------------------------------ Public files ------------------------------ */
+
+app.use(express.static(join(__dirname, 'public')));
 
 /* ------------------------------- Run server ------------------------------- */
 
