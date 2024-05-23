ef506c493bce8ae5c14bc8bee17d38d802dec8d5
Erickfer29 <erickdelgado0829@gmail.com>
2024-05-22 21:18:55 -0500

    Creamos la vista add y la ruta y el post del add

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -1,18 +1,35 @@
-import { Router } from 'express';
+import { Router } from 'express'
 import pool from '../database.js'
 
 const router = Router();
 
-router.get('/list', async (req, res) => {
-    try {
-        const [result] = await pool.query('SELECT * FROM personas');
-        res.render('personas/list', { personas: result })
+router.get('/add', (req, res)=>{
+    res.render('personas/add')
+});
 
+router.post('/add', async (req, res)=>{
+    try {
+        const { name, lastname, age} = req.body
+        console.log(req.body)
+        const newPersona = {
+            name, lastname, age
+        }
+        console.log(newPersona)
+        /* ------ await pool.query('INSERT INTO personas SET ?', [newPersona]); ----- */
+        /* ------------------------- res.redirect('/list'); ------------------------- */
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
+router.get('/list', async (req, res) => {
+    try {
+        const [result] = await pool.query('SELECT * FROM personas');
+        res.render('personas/list', { personas: result })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 router.get('/delete/:id', async (req, res) => {
     try {
         const { id } = req.params
@@ -22,5 +39,7 @@ router.get('/delete/:id', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+
 
 export default router;
